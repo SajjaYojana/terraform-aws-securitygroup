d080ed02fd468d3a41b37b79df1409ed87d2cd50
Yojana Sajja <yojana@Yojanas-Laptop.attlocal.net>
2024-07-29 12:15:03 -0500

    eksctl

--- variables.tf
+++ variables.tf
@@ -1,45 +1,77 @@
+# variable "project_name" {
+#   type = string
+# }
+
+# variable "environment" {
+#   type = string
+# }
+
+# variable "sg_name" {
+#   type = string
+# }
+
+# variable "sg_description" {
+#   type = string
+# }
+
+# variable "vpc_id" {
+#   type = string
+# }
+
+# variable "common_tags" {
+#   type = map
+# }
+
+# variable "sg_tags" {
+#   type = map
+#   default = {}
+# }
+
+# variable "outbound_rules" {
+#   type = list
+#   default = [
+#     {
+#         from_port = 0
+#         to_port = 0
+#         protocol = "-1" # all protocols
+#         cidr_blocks = ["0.0.0.0/0"]
+#     }
+#   ]
+# }
+
+# variable "ingress_rules" {
+#   type = list
+#   default = []
+# }
+
+
+# Kubernates with terraform 
+
 variable "project_name" {
-  type = string
+  
 }
-
 variable "environment" {
-  type = string
+  
 }
-
 variable "sg_name" {
-  type = string
+  #default = mongodb
 }
-
-variable "sg_description" {
-  type = string
-}
-
-variable "vpc_id" {
-  type = string
-}
-
 variable "common_tags" {
-  type = map
-}
-
-variable "sg_tags" {
-  type = map
   default = {}
+  type = map
 }
-
-variable "outbound_rules" {
-  type = list
-  default = [
-    {
-        from_port = 0
-        to_port = 0
-        protocol = "-1" # all protocols
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-  ]
+variable "sg_tags" {
+  default = {}
+  type = map
 }
-
-variable "ingress_rules" {
+variable "vpc_id" {
+  
+}
+variable "sg_description" {
+  #default = ""
+  type = string
+}
+variable "sg_ingress_rules" {
   type = list
   default = []
 }
